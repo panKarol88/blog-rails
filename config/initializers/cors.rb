96403e448273bf607a1473084d974cb819bec3ba
Karol Kamiński <karol.kaminski1990@gmail.com>
2022-09-30 16:26:23 +0200

    handle CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,13 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # allow do
+  #   origins '*'
+  #   resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  # end
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+  allow do
+    origins 'localhost:3000', '127.0.0.1:3000', 'https://karol-blog-react.herokuapp.com',
+            /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
 
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  end
+end
