fcf07fb7a8afc71d9b759a50886650ba81925008
Karol Kamiński <karol.kaminski1990@gmail.com>
2022-12-13 11:33:54 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create!(email: 'koncipor@gmail.com', password: 'oknols88', nickname: 'panKarol')
+
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: true, worth_reading: true)
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: true, worth_reading: false)
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: false, worth_reading: true)
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: true, worth_reading: false)
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: true, worth_reading: true)
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: false, worth_reading: false)
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: false, worth_reading: true)
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: true, worth_reading: false)
+Article.create!(label: 'aaa', description: 'eew ewe dewiojod djwei jweo', content: 'aaa', bg_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Capybara_portrait.jpg/1280px-Capybara_portrait.jpg', published: true, worth_reading: false)
 p 'OK'
